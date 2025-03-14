e77920547d8519a33e7cb468edd8affbff11dfc8
Rahul <rahul.mymail1@gmail.com>
2025-03-14 12:58:25 +0530

    feat: init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+import { LatestPost } from "@/app/_components/post";
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function Home() {
+  return (
+    <h1>
+      Hello World
+    </h1>
+  );
+}
