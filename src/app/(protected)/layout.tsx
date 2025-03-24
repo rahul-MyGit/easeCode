ce9ad52838e3b8500c6478f2dfe13f30d24d6e07
Rahul <rahul.mymail1@gmail.com>
2025-03-25 01:17:48 +0530

    feat: auth route

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,12 +1,28 @@
 import { Input } from '@/components/ui/input'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import AppSidebar from './appsidebar/appsidebar'
 
 const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='mt-14'>
-        <Input type="text" />
-        {children}
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+
+      <main className='w-full m-2'>
+        <div className='flex items-center ap-2 border-sidebar-border rounded-md'>
+          {/* <Searchbar /> */}
+          <div className='ml-auto'></div>
+          <UserButton />
+        </div>
+
+        <div className='h-4'></div>
+        {/* main content */}
+        <div className='border-sidebar-border bg-sidebar'>
+          {children}
+        </div>
+      </main>
+    </SidebarProvider>
   )
 }
 
