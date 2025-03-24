0932e55a9c9befba337e2821216bfff3f7418fdf
Rahul <rahul.mymail1@gmail.com>
2025-03-24 22:36:14 +0530

    feat: Add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
-
+import { Button } from "@/components/ui/button";
 export default async function Home() {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <Button onClick={() => {
+      alert("clicked");
+    }}>
+      Click me
+    </Button>
   );
 }
